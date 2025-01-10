76e04b6a02b8f73e3bcba68578d4adb522657282
Luis Roman <lromanp@utp.edu.pe>
2025-01-10 15:39:32 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ dotenv.config();
 const app = express();
 
 app.use(userRoutes);
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Permitir solo tu frontend local
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.listen(process.env.PORT, () => {
   console.log(`SERVER IS RUNNING IN PORT ${process.env.PORT}`);
 });
