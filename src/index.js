640abeaa2754d8f860ad640a9d070bf5a9a9f1ff
Luis Roman <lromanp@utp.edu.pe>
2025-01-10 15:50:43 -0500

    changes in cors

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(userRoutes);
+// Configuración de CORS debe ir antes de las rutas
 app.use(
   cors({
     origin: "http://localhost:5173", // Permitir solo tu frontend local
     methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Rutas deben ir después de configurar CORS
+app.use(userRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`SERVER IS RUNNING IN PORT ${process.env.PORT}`);
 });
