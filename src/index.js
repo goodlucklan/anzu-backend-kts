68286b5a972111139ed82c6ff9cdb5e023fadc7d
Luis Roman <goodluckxdc@gmail.com>
2025-08-25 21:42:59 -0500

    Add socket io in the code

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ import dotenv from "dotenv";
 import session from "express-session";
 import pgSession from "connect-pg-simple";
 import db from "../database/pg.sql.js";
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
 
 const app = express();
 const PgSession = pgSession(session);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -35,12 +46,25 @@ app.use(
   })
 );
 
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Message received:", data);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/tournament", tournamentRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`SERVER IS RUNNING IN PORT ${process.env.PORT}`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
